59b22028e2ed2f0cc8cd94e3f91fa4ad7a735af5
xinranxu0930 <xinranxu0930@gmail.com>
2025-02-18 15:18:34 +0800

    Modify flat_readlen

--- flat_readlen.py
+++ flat_readlen.py
@@ -2,7 +2,7 @@ import mappy as mp
 import argparse
 
 
-def get_splitread(sample_name, fq1, fq2, max_len):
+def get_splitread(sample_name, fq1, fq2, max_len, software_type):
     split_fq1_name = f"{sample_name}_R1.fq"
     split_fq2_name = f"{sample_name}_R2.fq"
     split_fq1 = open(split_fq1_name, "w")
@@ -21,8 +21,10 @@ def get_splitread(sample_name, fq1, fq2, max_len):
         seq2, qual2 = read2[1], read2[2]
 
         split_fq1.write(f"@{read_name} 1{comment}\n{seq1[:max_len]}\n+\n{qual1[:max_len]}\n")
-        # split_fq2.write(f"@{read_name} 2{comment}\n{seq2[:max_len]}\n+\n{qual2[:max_len]}\n")
-        split_fq2.write(f"@{read_name} 2{comment}\n{seq2}\n+\n{qual2}\n")
+        if software_type == "UMI4Cats":
+            split_fq2.write(f"@{read_name} 2{comment}\n{seq2[:max_len]}\n+\n{qual2[:max_len]}\n")
+        else:
+            split_fq2.write(f"@{read_name} 2{comment}\n{seq2}\n+\n{qual2}\n")
     split_fq1.close()
     split_fq2.close()
 
@@ -32,8 +34,9 @@ if __name__ == "__main__":
     parser.add_argument("-f1","--fastq1", type=str, help="R1 fastq file path")
     parser.add_argument("-f2","--fastq2", type=str, help="R2 fastq file path")
     parser.add_argument("-m","--max_len", type=int, help="read max length")
+    parser.add_argument("--software_type", choices=["UMI4Cats", "umi4cpackag"], required=True, help="选择分析软件: UMI4Cats 或 umi4cpackag")
     args = parser.parse_args()
 
     sample_name = args.fastq1.split("_tmp")[0]
 
-    get_splitread(sample_name, args.fastq1, args.fastq2, args.max_len)
+    get_splitread(sample_name, args.fastq1, args.fastq2, args.max_len, args.software_type)
